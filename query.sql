8803b8606f0fe53810390ceff2ae8a3d981c397e
akshat-khare <akshat.khare08@gmail.com>
2019-01-20 02:59:46 +0530

    q9 donee

--- query.sql
+++ query.sql
@@ -14,6 +14,21 @@ create view cs1160315_top20authorid as select * from cs1160315_authoridcount lim
 
 create view cs1160315_soleauthorpaperwithcount as select AuthorId, count(authorid) as co from cs1160315_soleauthorpaper, PaperByAuthors where cs1160315_soleauthorpaper.paperid = PaperByAuthors.paperid group by authorid order by co desc;
 create view cs1160315_targetqueryq6 as select authorid from cs1160315_soleauthorpaperwithcount where co>50;
+
+create view cs1160315_paperjvenue as select paper.paperid, paper.venueid, venue.type from paper, venue where paper.venueid = venue.venueid;
+create view cs1160315_journalpapers as select paperid from cs1160315_paperjvenue where type='journals';
+create view cs1160315_journalauthors as select distinct authorid from cs1160315_journalpapers ,paperbyauthors where cs1160315_journalpapers.paperid = paperbyauthors.paperid;
+create view cs1160315_targetqueryq7 as select * from cs1160315_allauthorid except select * from cs1160315_journalauthors;
+
+
+create view cs1160315_authorjpaperjvenue as select paperbyauthors.authorid, cs1160315_paperjvenue.paperid, cs1160315_paperjvenue.venueid, cs1160315_paperjvenue.type from paperbyauthors cross join cs1160315_paperjvenue where paperbyauthors.paperid = cs1160315_paperjvenue.paperid;
+create view cs1160315_journalauthorid as select distinct authorid from cs1160315_authorjpaperjvenue where type='journals';
+create view cs1160315_nonjournalauthorid as select distinct authorid from cs1160315_authorjpaperjvenue where type!='journals';
+create view cs1160315_onlyjournalauthorid as select * from cs1160315_journalauthorid except select * from cs1160315_nonjournalauthorid;
+
+create view cs1160315_authoridjpaperayear as select paperByAuthors.authorid, paper.paperid, paper.year from paperbyauthors cross join paper where paperbyauthors.paperid=paper.paperid;
+create view cs1160315_authorpaperyearcount as select authorid, year, count(paperid) as co from cs1160315_authoridjpaperayear group by authorid, year;
+create view cs1160315_targetqueryq9 as select distinct authorid from cs1160315_authorpaperyearcount where year=2012 and co>1 intersect select distinct authorid from cs1160315_authorpaperyearcount where year=2013 and co>2;
 --1--
 select type, count(type) as co from venue group by type order by co desc, type;
 --2--
@@ -26,7 +41,28 @@ select name from cs1160315_targetauthoridq4 left outer join author on cs1160315_
 select name from cs1160315_top20authorid left outer join author on cs1160315_top20authorid.authorid = author.authorid order by co desc, name;
 --6--
 select name from cs1160315_targetqueryq6 left outer join author on cs1160315_targetqueryq6.authorid = author.authorid order by name;
+--before this check the left outer joins, they are wrong probably--
+--7--
+select name from author, cs1160315_targetqueryq7 where author.authorid = cs1160315_targetqueryq7.authorid order by name;
+--8--
+select name from cs1160315_onlyjournalauthorid cross join author where cs1160315_onlyjournalauthorid.authorid=author.authorid order by name;
+--9--
+select name from cs1160315_targetqueryq9 cross join author where cs1160315_targetqueryq9.authorid = author.authorid order by name;
+--10--
+
+
 --CLEANUP--
+drop view cs1160315_targetqueryq9;
+drop view cs1160315_authorpaperyearcount;
+drop view cs1160315_authoridjpaperayear;
+drop view cs1160315_onlyjournalauthorid;
+drop view cs1160315_nonjournalauthorid;
+drop view cs1160315_journalauthorid;
+drop view cs1160315_authorjpaperjvenue;
+drop view cs1160315_targetqueryq7;
+drop view cs1160315_journalauthors;
+drop view cs1160315_journalpapers;
+drop view cs1160315_paperjvenue;
 drop view cs1160315_targetqueryq6;
 drop view cs1160315_soleauthorpaperwithcount;
 drop view cs1160315_top20authorid;
